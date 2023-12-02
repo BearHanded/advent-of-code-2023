a59d12c2213c2685995ea02f0b2da37af58cdfbe
bearHanded <bcm3223@gmail.com>
2023-12-02 11:02:08 -0500

    day 2

---
+++ 02/blocks.py
@@ -0,0 +1,48 @@
+from util import christmas_input
+
+INPUT = 'input.txt'
+TEST_INPUT = 'test_input.txt'
+
+
+def count_game(game_string):
+    # Parse and return game
+    parts = game_string.split(":")
+    game = {
+        "number": int(parts[0].split()[1]),
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+
+    for action in parts[1].split(";"):
+        for block in action.split(","):
+            block_details = block.split()
+            game[block_details[1]] = max(game[block_details[1]], int(block_details[0]))
+    return game
+
+
+def match_colors(game, red, green, blue):
+    return game["red"] <= red and game["green"] <= green and game["blue"] <= blue
+
+
+def cube_power(game):
+    return game["red"] * game["green"] * game["blue"]
+
+
+def possible_games(filename, red, green, blue):
+    lines = christmas_input.file_to_array(filename)
+    games = [count_game(line) for line in lines]
+    valid = [i for i in games if match_colors(i, red, green, blue)]
+    return sum(game["number"] for game in valid)
+
+
+def fewest_cubes(filename):
+    lines = christmas_input.file_to_array(filename)
+    games = [count_game(line) for line in lines]
+    return sum(cube_power(game) for game in games)
+
+
+assert possible_games(TEST_INPUT, 12, 13, 14) == 8
+print("Part One: ", possible_games(INPUT, 12, 13, 14))
+assert fewest_cubes(TEST_INPUT) == 2286
+print("Part Two: ", fewest_cubes(INPUT))
