ee0e94be3fbcae14e528f7e903f8c9ce23ddd305
bearHanded <bcm3223@gmail.com>
2023-12-10 21:38:51 -0500

    tiny cleanup

--- 10/pipes.py
+++ 10/pipes.py
@@ -1,6 +1,7 @@
 from util import christmas_input
 import numpy as np
 
+# Test Files
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 TEST_INPUT_2 = 'test_input2.txt'
@@ -10,7 +11,7 @@ TEST_INPUT_5 = 'test_input5.txt'
 TEST_INPUT_6 = 'test_input6.txt'
 
 
-# I sure dug a hole for myself...
+# Constants
 NORTH = "N"
 SOUTH = "S"
 EAST = "E"
@@ -19,7 +20,7 @@ NORTHWEST = "NW"
 NORTHEAST = "NE"
 SOUTHWEST = "SW"
 SOUTHEAST = "SE"
-directions = {
+DIRECTIONS = {
     NORTH: (0, -1),
     SOUTH: (0, 1),
     EAST: (1, 0),
@@ -29,13 +30,13 @@ directions = {
     SOUTHWEST: (-1, 1),
     SOUTHEAST: (1, 1),
 }
-inverted_directions = {
+INVERTED_DIRECTIONS = {
     NORTH: SOUTH,
     SOUTH: NORTH,
     EAST: WEST,
     WEST: EAST
 }
-pipes = {
+PIPES = {
     "|": (NORTH, SOUTH),
     "-": (EAST, WEST),
     "L": (NORTH, EAST),
@@ -51,17 +52,17 @@ def find_furthest(f):
     grid = [[i for i in list(line)] for line in christmas_input.file_to_array(f)]
     (y, x) = np.argwhere(np.array(grid) == "S").tolist()[0]
     distance = 0
-    curr_direction = pipes[find_start_piece(grid, x, y)][0]
+    curr_direction = PIPES[find_start_piece(grid, x, y)][0]
     curr_pipe = ""
     while curr_pipe != "S":
-        x1, y1 = directions[curr_direction]
+        x1, y1 = DIRECTIONS[curr_direction]
         x, y = x + x1, y + y1
         distance += 1
         curr_pipe = get_pipe(grid, x, y)
         if curr_pipe == "S":
             break
-        curr_direction = pipes[curr_pipe][0] if pipes[curr_pipe][1] == inverted_directions[curr_direction] \
-            else pipes[curr_pipe][1]
+        curr_direction = PIPES[curr_pipe][0] if PIPES[curr_pipe][1] == INVERTED_DIRECTIONS[curr_direction] \
+            else PIPES[curr_pipe][1]
 
     return int(distance/2)
 
@@ -69,21 +70,15 @@ def find_furthest(f):
 def find_start_piece(grid, start_x, start_y):
     connections = []
     for direction, in [NORTH, SOUTH, EAST, WEST]:
-        (x, y) = directions[direction]
+        (x, y) = DIRECTIONS[direction]
         pipe = get_pipe(grid, start_x + x, start_y + y)
         if pipe is None or pipe == ".":
             continue
-        if direction == NORTH and SOUTH in pipes[pipe]:
-            connections.append(NORTH)
-        elif direction == SOUTH and NORTH in pipes[pipe]:
-            connections.append(SOUTH)
-        elif direction == EAST and WEST in pipes[pipe]:
-            connections.append(EAST)
-        elif direction == WEST and EAST in pipes[pipe]:
-            connections.append(WEST)
+        if INVERTED_DIRECTIONS[direction] in PIPES[pipe]:
+            connections.append(direction)
 
-    for key in pipes:
-        if pipes[key] is not None and connections[0] in pipes[key] and connections[1] in pipes[key]:
+    for key in PIPES:
+        if PIPES[key] is not None and connections[0] in PIPES[key] and connections[1] in PIPES[key]:
             return key
 
 
@@ -95,42 +90,31 @@ def get_pipe(grid, x, y):
 
 def find_enclosed(f):
     original_grid = [[i for i in list(line)] for line in christmas_input.file_to_array(f)]
-
     (y, x) = np.argwhere(np.array(original_grid) == "S").tolist()[0]
     initial_piece = find_start_piece(original_grid, x, y)
     original_grid[y][x] = initial_piece  # Replace for later math
-
-    y *= 2  # prep for expansion ?????????????
+    y *= 2
     x *= 2
 
     (initial_y, initial_x) = (y, x)
-    curr_direction = pipes[initial_piece][0]
-    curr_pipe = ""
+    curr_direction = PIPES[initial_piece][0]
     grid = expand(original_grid)
 
     connected = set()
     while True:
-        x1, y1 = directions[curr_direction]
+        x1, y1 = DIRECTIONS[curr_direction]
         x, y = x + x1, y + y1
         curr_pipe = get_pipe(grid, x, y)
         connected.add((x, y))
         if x == initial_x and y == initial_y:
             break
-        curr_direction = pipes[curr_pipe][0] if pipes[curr_pipe][1] == inverted_directions[curr_direction] \
-            else pipes[curr_pipe][1]
+        curr_direction = PIPES[curr_pipe][0] if PIPES[curr_pipe][1] == INVERTED_DIRECTIONS[curr_direction] \
+            else PIPES[curr_pipe][1]
 
     empty = get_outer(grid, connected)
-    reduced_empty = list(filter(lambda pair: pair[0] % 2 == 0 and pair[1] % 2 == 0, empty))
-    reduced_connected = list(filter(lambda pair: pair[0] % 2 == 0 and pair[1] % 2 == 0, connected))
-    surrounded = len(original_grid) * len(original_grid[0]) - len(reduced_empty) - len(reduced_connected)
-
-    t = [["I"] * len(original_grid[0]) for i in range(len(original_grid))]
-    for x_0, y_0 in reduced_empty:
-        t[int(y_0/2)][int(x_0/2)] = "O"
-    for x_0, y_0 in reduced_connected:
-        t[int(y_0/2)][int(x_0/2)] = " "
-    for line in t:
-        print(line)
+    marked = empty | connected
+    compressed_marked = list(filter(lambda pair: pair[0] % 2 == 0 and pair[1] % 2 == 0, marked))
+    surrounded = len(original_grid) * len(original_grid[0]) - len(compressed_marked)
     return surrounded
 
 
@@ -153,9 +137,8 @@ def get_outer(grid, connected):
     growing = True
     while growing:
         next_empty = empty.copy()
-
         for (x, y) in empty:
-            for x1, y1 in directions.values():
+            for x1, y1 in DIRECTIONS.values():
                 new_x = x + x1
                 new_y = y + y1
                 if 0 < new_x < max_x and 0 < new_y < max_y and (new_x, new_y) not in connected:
@@ -174,7 +157,7 @@ def expand(grid):
         if y == (len(grid) - 1):
             break
         for x in range(len(grid[0])):
-            cell = "|" if SOUTH in pipes[grid[y][x]] and NORTH in pipes[grid[y+1][x]] else "."
+            cell = "|" if SOUTH in PIPES[grid[y][x]] and NORTH in PIPES[grid[y+1][x]] else "."
             expanded.append(cell)
         new_grid_y.append(expanded)
 
@@ -185,7 +168,7 @@ def expand(grid):
             expanded.append(new_grid_y[y][x])
             if x == (len(grid[0]) - 1):
                 break
-            cell = "-" if EAST in pipes[new_grid_y[y][x]] and WEST in pipes[new_grid_y[y][x+1]] else "."
+            cell = "-" if EAST in PIPES[new_grid_y[y][x]] and WEST in PIPES[new_grid_y[y][x+1]] else "."
             expanded.append(cell)
         new_grid_x.append(expanded)
 
