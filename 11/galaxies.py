217a5c9ead00041c2d4c32290df0397653eb4547
bearHanded <bcm3223@gmail.com>
2023-12-11 21:48:30 -0500

    smoller

--- 11/galaxies.py
+++ 11/galaxies.py
@@ -1,25 +1,20 @@
 from util import christmas_input
 import numpy as np
+from itertools import combinations
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 
 
-def find_shortest(f, expansion_size):
+def find_shortest(f, factor):
     stars = [list(line) for line in christmas_input.file_to_array(f)]
     rotated = [list(row) for row in zip(*stars[::-1])]
     expanded_cols = [idx for idx, row in enumerate(stars) if "#" not in row]
     expanded_rows = [idx for idx, row in enumerate(rotated) if "#" not in row]
-
     galaxies = np.argwhere(np.array(stars) == "#").tolist()
     galaxy_dist = {}
-    for i, galaxy in enumerate(galaxies):
-        y1, x1 = galaxy
-        for galaxy2 in galaxies[i+1:]:
-            y2, x2 = galaxy2
-            match_x = [row for row in expanded_rows if (x1 < row < x2) or (x2 < row < x1)]
-            match_y = [col for col in expanded_cols if (y1 < col < y2) or (y2 < col < y1)]
-            expanded_space = expansion_size * (len(match_x) + len(match_y)) - (len(match_x) + len(match_y))
-            galaxy_dist[(x1, y1), (x2, y2)] = abs(x1 - x2) + abs(y1 - y2) + expanded_space
+    for ((y1, x1), (y2, x2)) in list(combinations(galaxies, 2)):
+        expansion = len([row for row in expanded_rows if (x1 < row < x2) or (x2 < row < x1)]) + len([col for col in expanded_cols if (y1 < col < y2) or (y2 < col < y1)])
+        galaxy_dist[(x1, y1), (x2, y2)] = abs(x1 - x2) + abs(y1 - y2) + factor * expansion - expansion
     return sum(galaxy_dist.values())
 
 
