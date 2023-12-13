a6188e4a49ca6802d2d3bf535cc0028c12c58da7
bearHanded <bcm3223@gmail.com>
2023-12-12 23:28:00 -0500

    cache cache cache

--- 12/springs.py
+++ 12/springs.py
@@ -1,89 +1,48 @@
 from functools import lru_cache
-
 from util import christmas_input
+
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 
 
 def total_arrangements(f, expand=False):
-    sets = [[parts[0], (int(i) for i in parts[1].split(","))] for parts in [line.split() for line in christmas_input.file_to_array(f)]]
+    sets = [line.split() for line in christmas_input.file_to_array(f)]
     total = 0
-    idx = 0
     for puzzle, cells in sets:
-        idx += 1
-        print("\nPUZZLE:", idx, puzzle)
-
-        chunks = [i for i in puzzle.split(".") if i]
-        possibilities = []
-        for chunk in chunks:
-            possibilities.append(possible_solutions(chunk, max(cells)))
-
-        puzzle_options = process_possibilities(possibilities, stringify(cells), "")
-        total += puzzle_options
+        if expand:
+            expanded_puzzle = puzzle
+            expanded_cells = cells
+            for i in range(4):
+                expanded_puzzle += "?" + puzzle
+                expanded_cells += "," + cells
+            puzzle = expanded_puzzle
+            cells = expanded_cells
+        cells = [int(x) for x in cells.split(',')]
+        total += possible_solutions(tuple(puzzle), tuple(cells))
     return total
 
 
-@cache
-def possible_solutions(chunk, max_size):
-    if "?" not in chunk:
-        result = [len(i) for i in chunk.split(".") if i]
-        if all(i <= max_size for i in result):
-            return [result]
-        return []
-    solutions = []
-    solutions += possible_solutions(chunk.replace("?", "#", 1), max_size)
-    solutions += possible_solutions(chunk.replace("?", ".", 1), max_size)
-    return solutions
-
-
-def process_possibilities(possibilities, cells, running):
-    if running not in cells:
-        return 0
-    if len(possibilities) == 0:
-        return 1 if cells == running else 0
-
+@lru_cache()
+def possible_solutions(puzzle, cells):
     solutions = 0
-    node = possibilities[0]
-    for option in node:
-        if len(option) == 0:
-            solutions += process_possibilities(possibilities[1:], cells, running)
-            continue
-        new_running = running
-        if new_running:
-            new_running += ","
-        new_running += stringify(option)
-        solutions += process_possibilities(possibilities[1:], cells, new_running)
+    if len(cells) == 0:  # reached a match
+        return 1 if '#' not in puzzle else 0
+    elif sum(cells) + len(cells) - 1 > len(puzzle):
+        return 0
+    elif puzzle[0] == '.':  # skip
+        return possible_solutions(puzzle[1:], cells)
+    elif puzzle[0] == '?':  # look forward + consider current match
+        solutions += possible_solutions(puzzle[1:], cells)
+
+    cell_length = cells[0]
+    # current cell_length is valid for an item, look forward for next cell
+    if '.' not in puzzle[:cell_length] and (len(puzzle) <= cell_length or len(puzzle) > cell_length and puzzle[cell_length] != '#'):
+        solutions += possible_solutions(puzzle[cell_length + 1:], cells[1:])
+
     return solutions
 
-
-def stringify(int_array):
-    return ",".join([str(i) for i in int_array])
-
-
-# def unfold_arrangements(f):
-#     sets = [line.split() for line in christmas_input.file_to_array(f)]
-#     total = 0
-#     idx = 0
-#     for puzzle, cells in sets:
-#         expanded_puzzle, expanded_cells = puzzle, cells
-#         for i in range(4):
-#             expanded_puzzle += "?" + puzzle
-#             expanded_cells += "," + cells
-#         idx += 1
-#         print("\nPUZZLE:", idx, expanded_puzzle)
-#
-#         chunks = [i for i in expanded_puzzle.split(".") if i]
-#         possibilities = []
-#         for chunk in chunks:
-#             possibilities.append(possible_solutions(chunk, max( [int(i) for i in cells.split(",")])))
-#
-#         puzzle_options = process_possibilities(possibilities, stringify(expanded_cells), "")
-#         print("  ", puzzle_options)
-#         total += puzzle_options
-#     return total
-#
 
 assert total_arrangements(TEST_INPUT) == 21
-# print("Part One: ", total_arrangements(INPUT))
-
-# assert unfold_arrangements(TEST_INPUT) == 525152
+assert total_arrangements(TEST_INPUT, True) == 525152
+print("Part One: ", total_arrangements(INPUT))
+print("Part Two: ", total_arrangements(INPUT, True))
