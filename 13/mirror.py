5f6e2ec2c1b2c51c5c234d9c69da280d552b5154
bearHanded <bcm3223@gmail.com>
2023-12-13 23:35:43 -0500

    my d&d group cancelled on me so I wrote code instead

--- 13/mirror.py
+++ 13/mirror.py
@@ -4,7 +4,7 @@ INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 
 
-def part_one(f):
+def mirror_sum(f, clean_mirrors=False):
     patterns = np.array(christmas_input.file_to_array(f))
     groups = [[]]
     total_left = 0
@@ -16,20 +16,25 @@ def part_one(f):
             continue
         groups[-1].append(row)
     for group in groups:
-        for row in group:
-            print(row)
-        total_above += calc_mirrors(group)
-        total_left += calc_mirrors(["".join(i) for i in list(zip(*group[::-1]))])  # clockwise
+        total_above += calc_mirrors(group, clean_mirrors)
+        total_left += calc_mirrors(["".join(i) for i in list(zip(*group[::-1]))], clean_mirrors)  # clockwise
     return total_left + 100 * total_above
 
 
-def calc_mirrors(group):
+def calc_mirrors(group, clean_mirrors):
     for idx in range(len(group) - 1):
         size = min(idx + 1, len(group) - (idx + 1))
-        if "".join(group[:idx+1][-size:]) == "".join(group[idx+1:][:size][::-1]):
+        original = "".join(group[:idx+1][-size:])
+        reflection = "".join(group[idx+1:][:size][::-1])
+
+        if clean_mirrors and sum(1 for a, b in zip(original, reflection) if a != b) == 1:
+            return idx + 1
+        if not clean_mirrors and original == reflection:
             return idx + 1
     return 0
 
 
-assert part_one(TEST_INPUT) == 405
-print("Part One: ", part_one(INPUT))
+assert mirror_sum(TEST_INPUT) == 405
+print("Part One: ", mirror_sum(INPUT))
+assert mirror_sum(TEST_INPUT, True) == 400
+print("Part Two: ", mirror_sum(INPUT, True))
