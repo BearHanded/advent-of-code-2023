028ddf48805c2d255c41aca0d0cc82f23bd4b1e7
bearHanded <bcm3223@gmail.com>
2023-12-22 17:44:10 -0500

    closer?

--- 22/tetris.py
+++ 22/tetris.py
@@ -5,44 +5,49 @@ import time
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 
+
 def part_one(f):
     # Parse Bricks
-    bricks = [tuple(tuple([int(c) for c in i.split(",")]) for i in row.split("~")) for row in christmas_input.file_to_array(f)]
+    bricks = [tuple(tuple([int(c) for c in i.split(",")]) for i in row.split("~")) for row in
+              christmas_input.file_to_array(f)]
     bricks.sort(key=lambda tup: tup[0][2])
     bricks, supports = simulate(bricks)
     marked = [i for i in find_disintegrations(supports)]
     return len(marked)
 
+
 def part_two(f):
     # Parse Bricks
-    bricks = [tuple(tuple([int(c) for c in i.split(",")]) for i in row.split("~")) for row in christmas_input.file_to_array(f)]
+    bricks = [tuple(tuple([int(c) for c in i.split(",")]) for i in row.split("~")) for row in
+              christmas_input.file_to_array(f)]
     bricks.sort(key=lambda tup: tup[0][2])
     bricks, supports = simulate(bricks)
     marked = [i for i in find_disintegrations(supports)]
     cascade_sum = plan_cascade(bricks, supports, marked)
-            
+
     return cascade_sum
+
 
 def simulate(bricks):
     supports = {}
-    for idx in range(len(bricks)): 
+    for idx in range(len(bricks)):
         new_location, collisions = predict(bricks[idx], bricks, idx)
         supports[new_location] = collisions
         bricks[idx] = new_location
     return bricks, supports
-        
+
+
 def predict(b, bricks, curr_idx):
     z = 0  # ground level
     collisions = set()
-    settled = b
     for x in range(b[0][0], b[1][0] + 1):
         for y in range(b[0][1], b[1][1] + 1):
             for b2 in bricks[:curr_idx]:
-                if b2[0][0] <= x <= b2[1][0] and b2[0][1] <= y <= b2[1][1] : # Collision / Supported by
+                if b2[0][0] <= x <= b2[1][0] and b2[0][1] <= y <= b2[1][1]:  # Collision / Supported by
                     collisions.add(b2)
                     z = max(z, b2[1][2])
-    
-    z_delta = b[0][2] - z - 1 # Distance to block or ground
+
+    z_delta = b[0][2] - z - 1  # Distance to block or ground
     settled = ((b[0][0], b[0][1], b[0][2] - z_delta), (b[1][0], b[1][1], b[1][2] - z_delta))
     collisions = [i for i in collisions if i[1][2] == settled[0][2] - 1]  # compare top to bottom
     return settled, collisions
@@ -63,29 +68,35 @@ def find_disintegrations(supports):
 
 def plan_cascade(bricks, supports, marked):
     destroyable = copy.deepcopy(bricks)
+    bricks.sort(key=lambda tup: tup[1][2])  # Need to sort the soonest top edge
     for item in marked:
         destroyable.remove(item)
     total = 0
-    print(bricks)
+    # print(bricks)
     for b in destroyable:
-        destroyed = set([b])
+        print(b)
+        destroyed = {b}
         idx = bricks.index(b)
 
-        for b2 in bricks[idx+1:]:
+        for b2 in bricks[idx + 1:]:
+            print("   ", b2)
             if set(supports[b2]).issubset(destroyed):
+                print("      ", supports[b2])
+                print("      ", destroyed)
                 destroyed.add(b2)
-        print(destroyed)
+        # print(destroyed)
         total += len(destroyed) - 1  # TODO: Note the - 1, we are tracking the destroyed
 
     print(total)
     return total
+
 
 start = time.time()
 
 assert part_one(TEST_INPUT) == 5
 print("Part One: ", part_one(INPUT))
 assert part_two(TEST_INPUT) == 7
-print("Part Two: ", part_two(INPUT))
+# print("Part Two: ", part_two(INPUT))
 
 end = time.time()
 print(end - start)
