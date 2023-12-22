46a16007b008cddfc1f8db6800c71677bd084952
BearHanded <bcm3223@gmail.com>
2023-12-22 13:54:57 -0500

    efficient

--- 22/tetris.py
+++ 22/tetris.py
@@ -2,46 +2,64 @@ from util import christmas_input
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 
+# Looking for 375
 def part_one(f):
     # Parse Bricks
     bricks = [tuple(tuple([int(c) for c in i.split(",")]) for i in row.split("~")) for row in christmas_input.file_to_array(f)]
-    # bricks = dict([(i, brick) for (i, brick) in enumerate(bricks)])
-    bricks = simulate(bricks)
-    marked = find_disintegrations(bricks)
+    bricks.sort(key=lambda tup: tup[0][2])
+    bricks, supports = simulate(bricks)
+
+    # print("Supports:")
+    # for support in supports.items():
+    #     print(support)
+    marked = find_disintegrations(supports)
+    # print(len(marked))
+    # print("Marks:")
+    # for mark in marked:
+    #     print(mark)
     return len(marked)
 
 
 def simulate(bricks):
-    falling = True
-    while falling:
-        falling = False  # if only python had a do while loop
-        for idx in range(len(bricks)):  # we know our dict is indexed by #
-            new_location = predict(bricks[idx], bricks)
-            if new_location != bricks[idx]:
-                falling = True
-                bricks[idx] = new_location
-    return bricks
+    supports = {}
+    for idx in range(len(bricks)): 
+        new_location, collisions = predict(bricks[idx], bricks, idx)
+        supports[new_location] = collisions
+        if new_location != bricks[idx]:
+            bricks[idx] = new_location
+    return bricks, supports
         
-def predict(b, bricks):
+def predict(b, bricks, curr_idx):
     z = 0  # ground level
+    collisions = []
+    settled = b
+
     for x in range(b[0][0], b[1][0] + 1):
         for y in range(b[0][1], b[1][1] + 1):
-            for b2 in bricks:
-                if b == b2 or b[1][2] <= b2[1][2]:
-                    continue
-                if b2[0][0] <= x <= b2[1][0] and b2[0][1] <= y <= b2[1][1] : # Collision
+            for b2 in bricks[:curr_idx]:
+                if b2[0][0] <= x <= b2[1][0] and b2[0][1] <= y <= b2[1][1] : # Collision / Supported by
+                    collisions.append(b2)
                     z = max(z, b2[1][2])
     
-    z_delta = min(b[0][2], b[1][2]) - z - 1 # Distance to block or ground
+    z_delta = b[0][2] - z - 1 # Distance to block or ground
     if z_delta:
-        return ((b[0][0], b[0][1], b[0][2] - z_delta), (b[1][0], b[1][1], b[1][2] - z_delta))
-    return b
+        settled = ((b[0][0], b[0][1], b[0][2] - z_delta), (b[1][0], b[1][1], b[1][2] - z_delta))
+    collisions = [i for i in collisions if i[1][2] == settled[0][2] - 1]  # compare top to bottom
+    return settled, collisions
 
 
-def find_disintegrations(bricks):
-    # Build a map of what each brick is supported by
-    # If a brick does not appears in any support set alone, continue
-    return 0
+def find_disintegrations(supports):
+    marked = set()
+    for b in supports:
+        critical = False
+        for support_structure in supports.values():
+            if len(support_structure) == 1 and b in support_structure:
+                critical = True
+                break
+        if not critical:
+            marked.add(b)
+
+    return marked
 
 
 assert part_one(TEST_INPUT) == 5
