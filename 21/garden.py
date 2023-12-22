2b63f8bf75c9197ed4192f3ec07adaf2198804e2
bearHanded <bcm3223@gmail.com>
2023-12-21 21:34:34 -0500

    format

--- 21/garden.py
+++ 21/garden.py
@@ -1,9 +1,11 @@
 import heapq
 from util import christmas_input
 import numpy as np
+
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
 
 def avail_plots(f, max_steps):
     grid = [[c for c in row] for row in christmas_input.file_to_array(f)]
@@ -13,11 +15,12 @@ def avail_plots(f, max_steps):
     while len(queue) > 0:
         (steps, curr_idx) = heapq.heappop(queue)
 
-        next_moves = get_next(curr_idx, steps, grid, visited, max_steps) 
+        next_moves = get_next(curr_idx, steps, grid, visited, max_steps)
         for move in next_moves:
             heapq.heappush(queue, move)
-            
+
     return len([v for v in visited.values() if v % 2 == 0])
+
 
 def get_next(curr_idx, steps, grid, visited, max_steps):
     if steps >= max_steps:
@@ -27,14 +30,15 @@ def get_next(curr_idx, steps, grid, visited, max_steps):
     for move in DIRECTIONS:
         next_idx = (curr_idx[0] + move[0], curr_idx[1] + move[1])
         if (
-            not (0 <= next_idx[0] < len(grid[0]) and 0 <= next_idx[1] < len(grid))
-            or grid[next_idx[1]][next_idx[0]] == "#"
-            or next_idx in visited
+                not (0 <= next_idx[0] < len(grid[0]) and 0 <= next_idx[1] < len(grid))
+                or grid[next_idx[1]][next_idx[0]] == "#"
+                or next_idx in visited
         ):
             continue
         visited[next_idx] = next_steps  # Track being at the index
         next_moves.append((next_steps, next_idx))
     return next_moves
+
 
 assert avail_plots(TEST_INPUT, 6) == 16
 print("Part One: ", avail_plots(INPUT, 64))
--- 21/garden2.py
+++ 21/garden2.py
@@ -1,13 +1,15 @@
 import heapq
 from util import christmas_input
 import numpy as np
+
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
 DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+
 def avail_plots(lines, max_steps, start=None):
     grid = [[c for c in row] for row in lines]
-    if start == None:
+    if start is None:
         (y, x) = np.argwhere(np.array(grid) == "S").tolist()[0]
     else:
         (y, x) = start
@@ -17,7 +19,7 @@ def avail_plots(lines, max_steps, start=None):
     while len(queue) > 0:
         (steps, curr_idx) = heapq.heappop(queue)
 
-        next_moves = get_next(curr_idx, steps, grid, visited, max_steps) 
+        next_moves = get_next(curr_idx, steps, grid, visited, max_steps)
         for move in next_moves:
             heapq.heappush(queue, move)
     even = len([v for v in visited.values() if v % 2 == 0])
@@ -30,14 +32,15 @@ def avail_plots_scaled(lines, max_steps):
     (mid, _) = np.argwhere(np.array(grid) == "S").tolist()[0]
     boards = max_steps // len(grid)
 
-    _, state_1 = avail_plots(lines, mid)               # first edge
-    state_2, _ = avail_plots(lines, len(grid) + mid)   # next edge
-    _, state_3 = avail_plots(lines, 2*len(grid) + mid) # next edge
-    
+    _, state_1 = avail_plots(lines, mid)  # first edge
+    state_2, _ = avail_plots(lines, len(grid) + mid)  # next edge
+    _, state_3 = avail_plots(lines, 2 * len(grid) + mid)  # next edge
+
     b0 = state_1
-    b1 = state_2 - state_1 
+    b1 = state_2 - state_1
     b2 = state_3 - state_2
-    return (boards*(boards-1)//2)*(b2-b1) + b1*boards + b0
+    return (boards * (boards - 1) // 2) * (b2 - b1) + b1 * boards + b0
+
 
 def get_next(curr_idx, steps, grid, visited, max_steps):
     if steps >= max_steps:
@@ -47,12 +50,13 @@ def get_next(curr_idx, steps, grid, visited, max_steps):
     for move in DIRECTIONS:
         next_idx = (curr_idx[0] + move[0], curr_idx[1] + move[1])
         if (
-            grid[next_idx[1]%len(grid)][next_idx[0]%len(grid)] == "#"
-            or next_idx in visited
+                grid[next_idx[1] % len(grid)][next_idx[0] % len(grid)] == "#"
+                or next_idx in visited
         ):
             continue
         visited[next_idx] = next_steps  # Track being at the index
         next_moves.append((next_steps, next_idx))
     return next_moves
+
 
 print("Part Two: ", avail_plots_scaled(christmas_input.file_to_array(INPUT), 26501365))
