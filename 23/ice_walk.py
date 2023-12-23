b22ad03181939baa2b04bf2c30292cbf9d2256f5
bearHanded <bcm3223@gmail.com>
2023-12-23 14:06:55 -0500

    remove p2 from p1

--- 23/ice_walk.py
+++ 23/ice_walk.py
@@ -15,7 +15,7 @@ SLOPES = {
 START = (1, 0)
 
 
-def walk(grid, slippery=True):
+def walk(grid):
     visited = {}
     queue = PriorityQueue()
     queue.put((0, START, {START}))
@@ -23,19 +23,18 @@ def walk(grid, slippery=True):
 
     while not queue.empty():
         (steps, curr_idx, path) = queue.get()
-        next_moves = get_next(curr_idx, steps, grid, visited, path, slippery)
+        next_moves = get_next(curr_idx, steps, grid, visited, path)
         for move in next_moves:
             queue.put(move)
-    print(visited[end])
     return visited[end]
 
 
-def get_next(curr_idx, steps, grid, visited, path, slippery):
+def get_next(curr_idx, steps, grid, visited, path):
     if curr_idx == (len(grid[0]) - 2, len(grid) - 1):
         return []
     next_moves = []
     curr_tile = grid[curr_idx[1]][curr_idx[0]]
-    move_set = [SLOPES[curr_tile]] if (slippery and curr_tile in SLOPES) else DIRECTIONS
+    move_set = [SLOPES[curr_tile]] if curr_tile in SLOPES else DIRECTIONS
 
     for move in move_set:
         next_idx = (curr_idx[0] + move[0], curr_idx[1] + move[1])
@@ -44,7 +43,6 @@ def get_next(curr_idx, steps, grid, visited, path, slippery):
             not (0 <= next_idx[0] < len(grid[0]) and 0 <= next_idx[1] < len(grid))
             or grid[next_idx[1]][next_idx[0]] == "#"
             or next_idx in path
-            # or (next_idx in visited and visited[next_idx] > steps)
         ):
             continue
 
@@ -56,14 +54,9 @@ def get_next(curr_idx, steps, grid, visited, path, slippery):
     return next_moves
 
 
-assert_equals(walk((1, 0), file_to_array(TEST_INPUT)), 94)
-assert_equals(walk((1, 0), file_to_array(TEST_INPUT), False), 154)
+assert_equals(walk(file_to_array(TEST_INPUT)), 94)
 
 
 start = time.time()
-print("Part One: ", walk((1, 0), file_to_array(INPUT)))
-print(time.time() - start)
-
-start = time.time()
-print("Part Two: ", walk((1, 0), file_to_array(INPUT), False))
+print("Part One: ", walk(file_to_array(INPUT)))
 print(time.time() - start)
