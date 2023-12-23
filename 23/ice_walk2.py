946afea016c0754169c97cd84eab9cfbddd5c685
bearHanded <bcm3223@gmail.com>
2023-12-23 13:13:16 -0500

    long run, but it works

--- 23/ice_walk2.py
+++ 23/ice_walk2.py
@@ -15,22 +15,20 @@ SLOPES = {
 START = (1, 0)
 
 
-def walk(grid, slippery=True):
-    print(len(grid[0]), "x", len(grid))
-    visited = {}
-    queue = PriorityQueue()
-    queue.put((0, START, {START}))
+def walk(grid):
     end = (len(grid[0]) - 2, len(grid) - 1)
 
     intersections = build_intersections(grid, end)
     network = connect_graph(intersections, grid)
+    visited = dict([(i, 0) for i in intersections.keys()])
 
+    queue = PriorityQueue()
+    queue.put((0, START, {START}))
     while not queue.empty():
         (steps, curr_idx, path) = queue.get()
-        next_moves = get_next(curr_idx, steps, grid, visited, path, slippery)
+        next_moves = get_next(curr_idx, steps, network, visited, path, end)
         for move in next_moves:
             queue.put(move)
-    print(visited[end])
     return visited[end]
 
 
@@ -57,7 +55,6 @@ def build_intersections(grid, end):
 def connect_graph(intersections, grid):
     graph = {}
     for intersection, neighbors in intersections.items():
-        print("BUILDING", intersection)
         connections = []
         for neighbor in neighbors:
             path = {intersection}
@@ -79,47 +76,32 @@ def connect_graph(intersections, grid):
                     next_idx = candidate
 
             if next_idx:
-                print("  - attaching", next_idx, len(path))
                 connections.append((next_idx, len(path)))  # Don't count the source intersection
         graph[intersection] = connections
     return graph
 
 
-def get_next(curr_idx, steps, grid, visited, path, slippery):
-    if curr_idx == (len(grid[0]) - 2, len(grid) - 1):
+def get_next(curr_idx, steps, graph, visited, path, end):
+    if curr_idx == end:
         return []
     next_moves = []
-    curr_tile = grid[curr_idx[1]][curr_idx[0]]
-    move_set = [SLOPES[curr_tile]] if (slippery and curr_tile in SLOPES) else DIRECTIONS
 
-    for move in move_set:
-        next_idx = (curr_idx[0] + move[0], curr_idx[1] + move[1])
-        next_steps = steps + 1
-        if (
-            not (0 <= next_idx[0] < len(grid[0]) and 0 <= next_idx[1] < len(grid))
-            or grid[next_idx[1]][next_idx[0]] == "#"
-            or next_idx in path
-            # or (next_idx in visited and visited[next_idx] > steps)
-        ):
+    for neighbor, cost in graph[curr_idx]:
+        next_steps = steps + cost
+        if neighbor in path:
             continue
 
         next_path = path.copy()
-        next_path.add(next_idx)
-        visited[next_idx] = next_steps
-        next_moves.append((next_steps, next_idx, next_path))
-
+        next_path.add(neighbor)
+        visited[neighbor] = max(visited[neighbor], next_steps)
+        next_moves.append((next_steps, neighbor, next_path))
     return next_moves
 
 
 assert_equals(len(build_intersections(file_to_array(TEST_INPUT), (21, 22))), 9)
-assert_equals(walk(file_to_array(TEST_INPUT)), 94)
-assert_equals(walk(file_to_array(TEST_INPUT), False), 154)
+assert_equals(walk(file_to_array(TEST_INPUT)), 154)
 
 
 start = time.time()
-print("Part One: ", walk(file_to_array(INPUT)))
-print(time.time() - start)
-
-start = time.time()
-print("Part Two: ", walk(file_to_array(INPUT), False))
+print("Part Two: ", walk(file_to_array(INPUT)))
 print(time.time() - start)
